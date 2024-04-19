a6e332fb979de463720a35de9cf24214b612a184
Lucas Eifler <lucass.eifler@gmail.comm>
2024-04-19 12:13:05 +0200

    setting the first connection between server.js database.js.

---
+++ database.js
@@ -0,0 +1,17 @@
+const newsData = [
+    {
+      id: 1,
+      title: "Notícia 1",
+      content: "Conteúdo da Notícia 1",
+      date: "2024-04-15",
+    },
+    {
+      id: 2,
+      title: "Notícia 2",
+      content: "Conteúdo da Notícia 2",
+      date: "2024-04-14",
+    },
+    
+  ];
+  
+  module.exports = { newsData };
