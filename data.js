751337b17dda60763c7f6d0373863d120783e0b0
Jonas Ienco <jonasienco1@gmail.com>
2024-04-19 14:24:30 +0200

    set up server

---
+++ data.js
@@ -0,0 +1,19 @@
+const blogPosts = [
+    {
+        headline: "Milei government will break state monopoly on public transport cards",
+        image: "https://www.infomoney.com.br/wp-content/uploads/2021/06/cropped-argentina.jpg?w=640&quality=70&strip=all"
+    },
+    {
+        headline: "Chicago Fed President Sees Solid Economy",
+        image: "https://www.infomoney.com.br/wp-content/uploads/2019/06/federal-reserve-nova-york.jpg?w=750&quality=70&strip=all"
+    },
+    {
+        headline: "TSMC will expand factory in the USA",
+        image: "https://www.infomoney.com.br/wp-content/uploads/2024/01/406490035.jpg?w=1536&quality=70&strip=all"
+    },
+    {
+        headline: "Why is BitCoin falling?",
+        image: "https://www.infomoney.com.br/wp-content/uploads/2023/07/52781910748_eb61d5f682_c.jpg?w=799&quality=70&strip=all"
+    },
+];
+
--- database.js
+++ database.js
@@ -1,17 +1,25 @@
 const newsData = [
-    {
+  {
       id: 1,
-      title: "Notícia 1",
-      content: "Conteúdo da Notícia 1",
-      date: "2024-04-15",
-    },
-    {
+      headline: "Milei government will break state monopoly on public transport cards",
+      image: "https://www.infomoney.com.br/wp-content/uploads/2021/06/cropped-argentina.jpg?w=640&quality=70&strip=all"
+  },
+  {
       id: 2,
-      title: "Notícia 2",
-      content: "Conteúdo da Notícia 2",
-      date: "2024-04-14",
-    },
-    
-  ];
-  
+      headline: "Chicago Fed President Sees Solid Economy",
+      image: "https://www.infomoney.com.br/wp-content/uploads/2019/06/federal-reserve-nova-york.jpg?w=750&quality=70&strip=all"
+  },
+  {
+      id: 3,
+      headline: "TSMC will expand factory in the USA",
+      image: "https://www.infomoney.com.br/wp-content/uploads/2024/01/406490035.jpg?w=1536&quality=70&strip=all"
+  },
+  {
+      id: 4,
+      headline: "Why is BitCoin falling?",
+      image: "https://www.infomoney.com.br/wp-content/uploads/2023/07/52781910748_eb61d5f682_c.jpg?w=799&quality=70&strip=all"
+  },
+];
+
   module.exports = { newsData };
+  
