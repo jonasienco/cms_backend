35e7131d26f6c093b79b32a0cf928c18bf89349d
Jonas Ienco <jonasienco1@gmail.com>
2024-05-02 10:00:28 +0200

    changed styling to daisyUI lemonade

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-base-100 text-gray-300 text-center py-4">
-      PokeFight | Group 01 | WBS Cooding School 2024
-    </footer>
+    <div data-theme="lemonade">
+      <footer className="bg-base-100 text-gray-300 text-center py-4">
+        PokeFight | Group 01 | WBS Cooding School 2024
+      </footer>
+    </div>
   );
 };
 
