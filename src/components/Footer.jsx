3ccd5b887eeb10e62ca0c29f77dc334ee141509a
Jonas Ienco <jonasienco1@gmail.com>
2024-05-02 08:42:53 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
+import React from "react";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="bg-base-100 text-gray-300 text-center py-4">
+      PokeFight | Group 01 | WBS Cooding School 2024
+    </footer>
+  );
 };
 
 export default Footer;
